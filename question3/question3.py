7455ffd6d88363f2bb6a760a8df2061e64068908
Mehmet Can <mehmetyuney@marun.edu.tr>
2019-05-21 22:28:06 +0300

    question3

--- question3/question3.py
+++ question3/question3.py
@@ -4,7 +4,7 @@ import sys
 from os.path import join
 from scipy.io import wavfile
 from matplotlib import pyplot as plt
-from numpy.fft import fft, ifft, fftfreq
+from numpy.fft import fft, ifft, fftfreq, fftshift
 import scipy
 
 samplerate, data = wavfile.read(join("records", "input_record.wav"))
@@ -21,14 +21,11 @@ freqs = fftfreq(len(data), 1/samplerate)
 plt.figure(1)
 plt.plot(freqs, fftabs)
 
-filterfreq_upper = 100000
-filterfreq_lower = 1000
-for i in range(len(fftdata)):
-    tempabs = abs(fftdata[i])
-    if tempabs >= filterfreq_upper:
-        fftdata[i] = 0
-    if tempabs <= filterfreq_lower:
-        fftdata[i] = 0
+filter_size = 212000
+for i in range(filter_size + 1):
+    fftdata[220500 + i] = 0
+    fftdata[220500 - i] = 0
+
 
 ifftdata = ifft(fftdata).flatten()
 wavfile.write(join('outputs', 'output.wav'), samplerate, ifftdata.astype(data.dtype))
@@ -45,6 +42,14 @@ plt.show()
 # so after trim operation, that is why we can still the message that
 # is in the given wav file. However the low and high filter frequncies
 # depends on the signal, also depends on the voice level and gender.
+# However voice became more and more flat as we delete the data
+# because we're losing information although we can still hear what
+# is being said in the record.
+
+# if we set filter_size to 21800 voice is still hearable but not understandable
+# if we set filter_size to 21700 voice is hearable and understanable for a bit
+# if we set filter size to 21200 voice is both hearable and understanable
+# last 3 digit of filter_size is most likely to effect the voice's flatness
 
 # Wav file contains following text :
 # this is a test sound for cse three thousand and fourty eight introudction to signal and systems and my sound is loud and clear
