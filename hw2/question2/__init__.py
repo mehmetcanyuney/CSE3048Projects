2fd07bbed23edf824218787d0c00a1a1f81c1fe4
Mehmet Can <mehmetyuney@marun.edu.tr>
2019-06-06 00:44:28 +0300

    hw2

---
+++ hw2/question2/__init__.py
@@ -0,0 +1,132 @@
+import numpy as np
+from os.path import join
+from matplotlib.image import imread, imsave
+import matplotlib.pyplot as plt
+from PIL import Image
+from math import pow, sqrt
+from __util__ import initFilters, createAverageTemplate
+
+
+def MT(avg):
+    p = len(avg)
+    q = len(avg[0])
+
+    result = 0
+    for r in range(p):
+        for s in range(q):
+            result = result + avg[r][s]
+    result = result / (p * q)
+
+    return result
+
+
+def MI(x, y, avg, image):
+    p = len(avg)
+    q = len(avg[0])
+
+    result = 0
+    for r in range(p):
+        for s in range(q):
+            result = result + image[x + r][y + s]
+    result = result / (p * q)
+
+    return result
+
+
+def constCalc(avg, mt):
+    p = len(avg)
+    q = len(avg[0])
+
+    result = 0
+    for r in range(p):
+        for s in range(q):
+            temp = avg[r][s] - mt
+            result = result + pow(temp, 2)
+    result = sqrt(result)
+
+    return result
+
+
+def A(x,y, avg, image, mt, cons):
+    p = len(avg)
+    q = len(avg[0])
+
+    temp = 0
+    for r in range(p):
+        for s in range(q):
+            temp = temp + float((avg[r][s] - mt) * (image[x + r][y + s] - MI(x, y, avg, image)))
+    proportion = temp
+
+    temp = 0
+    for r in range(p):
+        for s in range(q):
+            temp = temp + pow((image[x + r][y + s] - MI(x, y, avg, image)), 2)
+    temp = sqrt(temp)
+
+    denominator = cons * temp
+
+    result = proportion / denominator
+
+    print("Similarity => ", result)
+    return result
+
+
+def rgb2gray(rgb):
+    temp = np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+    for i in range(len(temp)):
+        for j in range(len(temp[0])):
+            temp[i][j] = int(temp[i][j])
+    return temp
+
+
+# initialize the filters -> transform the image to grayscale and resize
+initFilters()
+# creates the average template by taking average of given 4 train image
+createAverageTemplate()
+
+for i in range(2,3,1):
+    input_path = "input" + str(i) + ".jpg"
+    output_path = input_path.replace('input', 'output')
+    print(input_path)
+
+    image = imread(input_path)
+    image = rgb2gray(image)
+    train1 = imread('train1.png')
+    train2 = imread('train2.png')
+    train3 = imread('train3.png')
+    train4 = imread('train4.png')
+    avg_train = imread('avg_train.jpg')
+
+    mt = MT(avg_train)
+    const_denomitator = constCalc(avg_train, mt)
+
+    pixel_jump = 30
+    similarities = []
+    for i in range(0, (len(image) - len(avg_train)), pixel_jump):
+        for j in range(0, (len(image[0]) - len(avg_train[0])), pixel_jump):
+            similarities.append([i, j, A(i, j, avg_train, image, mt, const_denomitator)])
+
+    best = -1
+    fx = None
+    fy = None
+    for i in similarities:
+        if i[2] > best:
+            best = i[2]
+            fx = i[0]
+            fy = i[1]
+
+    print(fx, fy)
+
+    image = np.array(image)
+
+    recty = 100
+    rectx = 120
+
+    for i in range(recty + 1):
+        image[fx][fy + i] = 0
+        image[fx + rectx][fy + i] = 0
+    for j in range(rectx + 1):
+        image[fx + j][fy] = 0
+        image[fx + j][fy + recty] = 0
+
+    imsave(output_path, image, cmap='gray', vmin=0, vmax=256)
---
+++ hw3/__init__.py
@@ -0,0 +1,124 @@
+import numpy as np
+import wave
+import sys
+from os.path import join
+from scipy.io import wavfile
+from matplotlib import pyplot as plt
+from numpy.fft import fft, ifft, fftfreq, fftshift
+from matplotlib.widgets import Slider
+import scipy
+
+samplerate, data = wavfile.read("allthree.wav")
+times = np.arange(len(data))/float(samplerate)
+
+fftdata = fft(data)
+fftabs = abs(fftdata)
+freqs = fftfreq(len(data), 1/samplerate)
+
+plt.figure(1)
+plt.plot(freqs, fftabs)
+
+plt.figure(2)
+plt.plot(fftabs)
+
+# 1. sound
+fftdata1 = fft(data)
+filter_size = 160000
+for i in range(filter_size + 1):
+    fftdata1[176400 + i] = 0
+    fftdata1[176400 - i] = 0
+
+fftabs1 = abs(fftdata1)
+plt.figure(3)
+plt.plot(freqs, fftabs1)
+
+ifftdata1 = ifft(fftdata1).flatten()
+wavfile.write(join('outputs', 'output1.wav'), samplerate, ifftdata1.astype(data.dtype))
+
+# 2.sound
+fftdata2 = fft(data)
+for i in range(len(fftdata2)):
+    if (i >= 32800 and i <= 92800) or (i >= 260000 and i <= 320000):
+        continue
+    fftdata2[i] = 0
+
+fftabs2 = abs(fftdata2)
+plt.figure(4)
+plt.plot(freqs, fftabs2)
+
+ifftdata2 = ifft(fftdata2)
+modulator2 = np.sin(2.0 * np.pi * 9140630 * times)
+res2 = []
+for i in range(len(ifftdata2)):
+    res2.append(ifftdata2[i] * modulator2[i])
+retfftdata2 = fft(res2)
+
+retfftabs2 = abs(retfftdata2)
+fig = plt.figure(5)
+a_plot, = plt.plot(freqs, retfftabs2)
+
+filter_size = 160000
+for i in range(filter_size + 1):
+    retfftdata2[176400 + i] = 0
+    retfftdata2[176400 - i] = 0
+
+ifftdata2 = ifft(retfftdata2).flatten()
+wavfile.write(join('outputs', 'output2.wav'), samplerate, ifftdata2.astype(data.dtype))
+
+
+# 3.sound
+fftdata3 = fft(data)
+for i in range(len(fftdata3)):
+    if (i >= 93800 and i <= 151800) or (i >= 201000 and i <= 259000):
+        continue
+    fftdata3[i] = 0
+
+fftabs3 = abs(fftdata3)
+plt.figure(6)
+plt.plot(freqs, fftabs3)
+
+ifftdata3 = ifft(fftdata3)
+modulator3 = np.sin(2.0 * np.pi * 64070 * times)
+res3 = []
+for i in range(len(ifftdata3)):
+    res3.append(ifftdata3[i] * modulator3[i])
+retfftdata3 = fft(res3)
+
+retfftabs3 = abs(retfftdata3)
+fig = plt.figure(7)
+a_plot, = plt.plot(freqs, retfftabs3)
+
+# slider_ax = plt.axes([0.1, 0.2, 0.4, 0.3])
+# a_slider = Slider(slider_ax,      # the axes object containing the slider
+#                   'a',            # the name of the slider parameter
+#                   10,          # minimal value of the parameter
+#                   400000 ,          # maximal value of the parameter
+#                   valinit=100  # initial value of the parameter
+#                  )
+#
+# def update(a):
+#     ifftdata3 = ifft(fftdata3)
+#     modulator3 = np.sin(2.0 * np.pi * a * times)
+#     res3 = []
+#     for i in range(len(ifftdata3)):
+#         res3.append(ifftdata3[i] * modulator3[i])
+#     retfftdata3 = fft(res3)
+#
+#     retfftabs3 = abs(retfftdata3)
+#
+#     a_plot.set_ydata(retfftabs3)
+#     fig.canvas.draw_idle()
+#
+# a_slider.on_changed(update)
+
+filter_size = 160000
+for i in range(filter_size + 1):
+    retfftdata3[176400 + i] = 0
+    retfftdata3[176400 - i] = 0
+
+ifftdata3 = ifft(retfftdata3).flatten()
+wavfile.write(join('outputs', 'output3.wav'), samplerate, ifftdata3.astype(data.dtype))
+
+
+
+plt.show()
